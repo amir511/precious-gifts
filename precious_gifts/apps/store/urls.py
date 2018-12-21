135bbb62995ca784126eb8510bd6a9f889c7bd68
Amir <amir.anwar.said@gmail.com>
2018-12-21 22:40:26 +0200

    More Views added

--- precious_gifts/apps/store/urls.py
+++ precious_gifts/apps/store/urls.py
@@ -1,8 +1,13 @@
 from django.conf.urls import url
-from precious_gifts.apps.store.views import ProductList, ProductDetail
+from precious_gifts.apps.store import views
 
+app_name = 'store'
 
 urlpatterns = [
-    url(r'^products/$', ProductList.as_view(), name='product_list'),
-    url(r'^product/(?P<pk>\d+)/$', ProductDetail.as_view(), name='product_detail'),
+    url(r'^products/$', views.ProductList.as_view(), name='product_list'),
+    url(r'^product/(?P<pk>\d+)/$', views.ProductDetail.as_view(), name='product_detail'),
+    url(r'^cart/view/$', views.view_cart, name='view_cart'), 
+    url(r'^cart/add/(?P<product_pk>\d+)/(?P<quantity>\d+)/$', views.add_to_cart, name='add_to_cart'), 
+    url(r'^cart/remove/(?P<product_pk>\d+)/$', views.remove_from_cart, name='remove_from_cart'), 
+
 ]
--- precious_gifts/apps/store/views.py
+++ precious_gifts/apps/store/views.py
@@ -1,6 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.core.exceptions import ObjectDoesNotExist
+from django.contrib import messages
 from django.views.generic import ListView, DetailView
-from precious_gifts.apps.store.models import Product
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.decorators import login_required
+from precious_gifts.apps.store.models import Product, Cart
 
 class ProductList(ListView):
     model = Product
@@ -9,4 +13,40 @@ class ProductList(ListView):
 class ProductDetail(DetailView):
     model = Product
     context_object_name = 'product'
+
+@login_required
+def view_cart(request):
+    try:
+        cart = request.user.cart
+    except ObjectDoesNotExist:
+        cart = Cart(user=request.user)
+        cart.save()
+
+    return render(request, 'store/view_cart.html', {'cart':cart})
+    
+@login_required
+def add_to_cart(request, product_pk, quantity):
+    try:
+        cart = request.user.cart
+    except ObjectDoesNotExist:
+        cart = Cart(user=request.user)
+    
+    product = Product.objects.get(pk=product_pk)
+    try:
+        cart.add_item(product, int(quantity))
+        cart.save()
+    except Exception as e:
+        messages.error(request, e)
+
+    return redirect('store:view_cart')
+
+@login_required
+def remove_from_cart(request, product_pk):
+    
+    cart = request.user.cart
+    product = Product.objects.get(pk=product_pk)
+    cart.remove_item(product)
+    cart.save()
+
+    return redirect('store:view_cart')
 
